016fbe3ace8d371233327a883ec2a901e477225a
Frank Girse <fgirse@mac-mini-von-frank.home>
2024-12-31 12:40:31 +0100

    10er commit

--- src/app/[locale]/(site)/page.tsx
+++ src/app/[locale]/(site)/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
       <ColorChangeCards />
       <CallToAct />
       <HorizontalScroll />
-      <Footer /> c                                        
+      <Footer />                                       
     </main>
   );
 }
