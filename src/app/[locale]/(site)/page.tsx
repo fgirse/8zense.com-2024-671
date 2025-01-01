74a1efbde62bd9a85a8810aa60380b325b624b0d
Frank Girse <fgirse@mac-mini-von-frank.home>
2025-01-01 15:11:50 +0100

    commit 13er

--- src/app/[locale]/(site)/page.tsx
+++ src/app/[locale]/(site)/page.tsx
@@ -3,8 +3,14 @@
 import Footer from "@/src/components/layout/Footer";
 import Navbar from "@/src/components/layout/Navbar/navbar";
 import Benefits from "@/src/sections/Benefits";
-import Header from "@/src/sections/Header";
 import HowItWorks from "@/src/sections/HowItWorks";
+import AuthInfo from "@/src/components/layout/AuthInfo/AuthInfoBar";
+import IntroWebside from "@/src/sections/Intro";
+import Hero from "@/src/sections/Hero";
+import FeatureAnimated from "@/src/sections/FeatureAnimated";
+import CollapseCardFeatures from "@/src/sections/ColllapsCardFeatures";
+import ColorChangeCards from "@/src/sections/ColorChangeCards";
+import CallToAct from "@/src/sections/CallToAct";
 //import Pricing from "@/src/sections/Pricing";
 //import Testimonials from "@/sections/Testimonials";
 //import WhyUs from "@/sections/WhyUs";
@@ -13,9 +19,14 @@ export default function Home() {
 	return (
 		<main>
 			<div className="bg-[#0D121F] px-[100px] text-white">
-				<Navbar />
-				<Header />
+			<AuthInfo />
+      	<Navbar />
+				
 			</div>
+      <div className="bg-[#0D121F] px-[2.5vw] lg:px-[1.vw] text-white">
+        <Hero />
+        <IntroWebside />
+      </div>
 			<Benefits />
 			<HowItWorks />
 		
@@ -44,7 +55,7 @@ export default function Home() {
       <div className="bg-[#0D121F] px-[2.5vw] lg:px-[1.vw] text-white">
         <Hero />
       </div>
-      <IntroWebside />
+   ©
       <section className="mt-60 bg-[#0D121F] px-[2.5vw] lg:px-[1.0vw] text-white">
       <CollapseCardFeatures />
       </section>
