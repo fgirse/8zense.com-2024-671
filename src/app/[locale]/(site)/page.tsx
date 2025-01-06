02f68ac797d493577a291ec42af6818fdef65d63
Frank Girse <fgirse@mac-mini-von-frank.home>
2025-01-06 12:18:20 +0100

    commit 26

--- src/app/[locale]/(site)/page.tsx
+++ src/app/[locale]/(site)/page.tsx
@@ -2,30 +2,26 @@
 
 import IntroWebside from "@/src/sections/Intro";
 import Hero from "@/src/sections/Hero";
-import FeatureAnimated from "@/src/sections/FeatureAnimated";
+import FeatureAnimated from "@/src/sections/FeatureAnimated/indextemp";
 import ColorChangeCards from "@/src/sections/ColorChangeCards";
 import CallToAct from "@/src/sections/CallToAct";
 import HorizontalScroll from "@/src/components/HorizontalScrollCarousel";
 import CollapseCardFeatures from "@/src/sections/ColllapsCardFeatures";
 
 export default function Page() {
-	return (
-	<>
-		
+  return (
+    <>
       <div className="bg-[#7c7a76] px-[2.5vw] lg:px-[1.vw] text-white">
         <Hero />
         <IntroWebside />
         <div className="bg-[#0D121F] px-[2.5vw] lg:px-[1.vw] text-white">
-        <CollapseCardFeatures/>
-        <FeatureAnimated />
-        <ColorChangeCards />
-        <CallToAct />
-<HorizontalScroll />        
+          <CollapseCardFeatures />
+          <FeatureAnimated />
+          <ColorChangeCards />
+          <CallToAct />
+          <HorizontalScroll />
         </div>
       </div>
-	
-		
-			
-		</>
-	);
+    </>
+  );
 }
--- src/sections/FeatureAnimated/indextemp.tsx
+++ src/sections/FeatureAnimated/indextemp.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect, useCallback } from "react";
 import { motion } from "framer-motion";
-import { Player } from "@lottiefiles/react-lottie-player";
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
 import { Link, Locale } from "@/src/i18n/routing";
 import { useLocale, useTranslations } from "next-intl";
 import getMessages from "next-intl/server";
@@ -24,38 +24,38 @@ export default function HowItWorks() {
       id: 1,
       text: t("text_01"),
       animation:
-        "https://lottie.host/e90fc516-078a-4e65-ac43-ab5a7b1bc4ed/W6DDrvJFL3.json",
+        "/images/lotti01.json",
     },
     {
       id: 2,
       text: t("text_02"),
       animation:
-        "https://lottie.host/57443a3d-e280-4f39-9134-86d96cd8f28f/ohtp2XVGjS.json",
+        "/images/lotti02.json",
     },
     {
       id: 3,
       text: t("text_03"),
       animation:
-        "https://lottie.host/78373cd4-e991-482b-8f25-e897acb9bbb8/V0IaYYRt3A.json",
+        "/images/lotti03.json",
     },
     {
       id: 4,
       text: t("text_04"),
       animation:
-        "https://assets3.lottiefiles.com/private_files/lf30_p9it5a2a.json",
+        "/images/lotti04.json",
     },
     {
       id: 5,
       text: t("text_05"),
       animation:
-        "https://lottie.host/d969d744-bf03-4aa4-90ac-50f9c5dda9b5/P36NGczhaX.json",
+        "/images/lotti05.json",
     },
 
     {
       id: 6,
       text: t("text_06"),
       animation:
-        "https://lottie.host/59703381-ed25-463c-913f-7cd163db2aa7/uF1vDxwgIZ.json",
+        "/images/lotti06.json",
     },
   ];
 
@@ -70,11 +70,13 @@ export default function HowItWorks() {
 
   return (
     <>
-      <h1 className="text-center bg-black uppercase text-3xl text-white lg:py-5">
+      <h1 className="text-center bg-black uppercase text-xl text-white lg:py-5">
         {t("Howitworks")}
+       
       </h1>
+      <h1 className="mt-7 text-center text-xl"> {t('Prozesse')} </h1>
       <motion.div
-        className=" flex flex-col sm:min-h-screen h-full"
+        className="bg-stone-300 flex flex-col "
         key={steps[step].id} // Add a unique key to trigger motion animation on step change
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
@@ -83,17 +85,15 @@ export default function HowItWorks() {
           duration: 1, // Adjust the duration as desired
         }}
       >
-        <div className="h-[10%] p-5 flex flex-col items-center justify-center">
+        <div className=" flex flex-col items-center justify-center">
           <div className=" grid-cols-2 gap-8">
-            <p className="mb-1§2 text-sm md:text-base text-slate-800 font-light leading-relaxed text-center"></p>
-            <h2 className="text-1xl md:text-4xl text-[#1A2238] font-semibold mb-4 text-center lg:py-9">
-              {`so funktioniert 8zense.com!!!`}
-            </h2>
+            <p className="mb-12 text-sm md:text-base text-slate-800 font-light leading-relaxed text-center"></p>
+            
             <Player
               autoplay
               loop
               src={steps[step].animation}
-              style={{ height: "26.66vh", width: "100%", marginBottom: "3vh" }}
+              style={{ height: "", width: "100%", marginBottom: "3vh" }}
             />
           </div>
           <p className="h-24 w-96 relative')]  text-sm px-6 text-slate-900 font-light mb-4 text-center">
